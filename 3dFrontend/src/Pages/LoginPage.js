2cb15c9ae78f9c5d7d906a331141329d77ec74a4
Adrian Tabacaru <43089315+DetectiveToaster@users.noreply.github.com>
2025-06-29 22:49:37 +0200

    Improve styles and add auth page

--- 3dFrontend/src/Pages/LoginPage.js
+++ 3dFrontend/src/Pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import '../styles/AuthPage.css';
 
 function LoginPage() {
   const { login } = useAuth();
--- 3dFrontend/src/Pages/RegisterPage.js
+++ 3dFrontend/src/Pages/RegisterPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import '../styles/AuthPage.css';
 
 function RegisterPage() {
   const { register } = useAuth();
