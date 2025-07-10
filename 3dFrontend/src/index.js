419833287157d87d85fd0c9408b829b9083eadd0
Adrian Tabacaru <43089315+DetectiveToaster@users.noreply.github.com>
2025-07-10 18:51:42 +0200

    Enable PWA support

--- 3dFrontend/src/index.js
+++ 3dFrontend/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import './theme.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
 
@@ -24,3 +25,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
